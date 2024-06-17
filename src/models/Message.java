465679e253078695f6fe047086e5d4e9540f07e3
mizuki.sugiyama <sugiyama-m@asfftt.co.jp>
2024-06-17 16:16:50 +0900

    ページネーションを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -11,18 +11,11 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
-
-
-
-
 @Entity
 
 @NamedQueries({
-    @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-    )
+        @NamedQuery(name = "getAllMessages", query = "SELECT m FROM Message AS m ORDER BY m.id DESC"),
+        @NamedQuery(name = "getMessagesCount", query = "SELECT COUNT(m) FROM Message AS m")
 })
 
 @Table(name = "tasks")
